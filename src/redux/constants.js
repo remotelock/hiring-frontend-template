2032d161598919baac545c185240db1fe74a318c
Pereverzev Yaroslav <pereverzevyi@gmail.com>
2021-08-20 17:45:49 +0300

    add constants

---
+++ src/redux/constants.js
@@ -0,0 +1,13 @@
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
+export const REQUEST = "REQUEST";
+
+export const GET_USERS = "GET_USERS";
+export const GET_DEVICES = "GET_DEVICES";
+
+export const ACCESS_FROM = "ACCESS_FROM";
+
+export const TOGGLE_LOCK = "TOGGLE_LOCK";
+
+export const userAPI = "http://localhost:4000/api/users.json";
+export const devicesAPI = "http://localhost:4000/api/devices.json";
