23cc8dc7b45a56eb06f63439049994f8c737ba21
Dunford Mainor <danmainor@outlook.com>
2022-02-17 05:19:02 +0300

    FEAT: Created and updated common module Strings with necessary global alert messages

---
+++ mobile/src/common/Strings/index.js
@@ -0,0 +1,18 @@
+const Strings = {
+    English: {
+      Alerts: {
+        CommonAlerts: {
+          no_internet: () => ({
+            title: '🌐 No Internet Connection 🌐',
+            message: 'Dear customer, your device is having some trouble communicating with our servers. Please make sure you have an active internet connection and retry. If issue persists, contact support from the button below​',
+            buttonText: 'OK, I Will',
+          }),
+          missing_auth: () => ({
+            title: '🔥 Unauthorized Request 🔥',
+            message: 'Dear customer, we are unable to complete your activation request. Please ensure you have an ACTIVE INTERNET CONNECTION or restart your phone if issue persists',
+            buttonText: 'OK, I Will',
+          }),
+        }
+    }
+}
+}
