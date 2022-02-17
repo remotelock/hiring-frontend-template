7003ef21e85cbe28c489dc22a888820f182edaf4
Dunford Mainor <danmainor@outlook.com>
2022-02-17 05:18:06 +0300

    FEAT: Created and updated common module Media with app Media assets exports

---
+++ mobile/src/common/Media/index.js
@@ -0,0 +1,8 @@
+const Images = {
+    AppIcon: require('../../assets/icon.png'),
+    AppIconAdaptive: require('../../assets/adaptive-icon.png'),
+    SplashScreen: require('../../assets/splash.png'),
+    Favicon: require('../../assets/favicon.png'),
+  }
+  export default Images
+  
