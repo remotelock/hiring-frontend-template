8fb2314de2fe9450043e60999ee64d0b7a82aa51
Dunford Mainor <danmainor@outlook.com>
2022-02-17 05:05:32 +0300

    CHORE: Updated babel config to include metro preset & module-resolver plugin

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,7 +1,22 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [["module-resolver", { root: ["."] }]],
+  presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      ["module-resolver", {
+        root: ["./src"],
+        alias: {
+          "@assets": "./src/assets",
+          "@common": "./src/common",
+          "@pages": "./src/app/pages",
+          "@components": "./src/app/components",
+          "@actioncreators": "./src/redux/actioncreators",
+          "@reducers": "./src/redux/reducers",
+          "@services": "./src/services"
+        },
+        extensions: [".ts", ".js", ".jsx", ".json"]
+    }],
+    ['@babel/plugin-proposal-export-namespace-from'],
+  ],
   };
 };
