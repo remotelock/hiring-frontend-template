28a1d02121006ba6131ae20d86c8121a0838f1dc
Dunford Mainor <danmainor@outlook.com>
2022-02-17 05:15:45 +0300

    FEAT: Created and updated common module Colors with necessary Color assets

---
+++ mobile/src/common/Colors/index.js
@@ -0,0 +1,35 @@
+const Colors = {
+    // Reds
+    Red: '#F80000',
+    Orange: '#DB4502',
+    Maroon: '#AA1932',
+    Magenta: '#E30074',
+    TransRed: '#F800004D',
+    InstagramMagenta: '#C32AA3',
+    // Greens
+    Green: '#085A2B',
+    LightGreen: '#ABDCC3',
+    JungleGreen: '#098C59',
+    TransGreen: '#085A2B4D',
+    WhatsAppGreen: '#25D366',
+    // Blues
+    Blue: '#1517BB',
+    DarkBlue: '#0E1082',
+    LightBlue: '#4345C8',
+    TwitterBlue: '#1DA1F2',
+    FacebookBlue: '#1877F2',
+    // Black and White
+    Gray: '#808080',
+    White: '#FFFFFF',
+    Black: '#000000',
+    DarkGray: '#222222',
+    LightGray: '#E1E1E1',
+    OffWhite: '#FAFAFA',
+    LightBackground: '#EEEEEE',
+    TransBlack: '#0000004D',
+    DarkOverlay: '#000000CC',
+    DarkBackground: '#1A1A1A',
+    Transparent: '#FFFFFF00',
+  }
+  export default Colors
+  
