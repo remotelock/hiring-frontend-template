f9387a30a898f929184e0b2c5bfd31ec0dc903a2
Dunford Mainor <danmainor@outlook.com>
2022-02-17 05:27:04 +0300

    FEAT: Created updated app entry point after ejecting from expo

---
+++ mobile/index.js
@@ -0,0 +1,6 @@
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+
+import Root from './src/app/container/Root';
+
+AppRegistry.registerComponent('main', () => Root);
